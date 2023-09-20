d5650570fab7649ee6a00f4dde20c07980039535
Lylithrea <ezraduss@gmail.com>
2023-09-20 19:02:12 +0200

    updated movement system

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,19 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float movementSpeed;
     [SerializeField] private float rotationSpeed;
+    public CharacterController controller;
+    private Vector3 moveDirection = Vector3.zero;
 
-
-
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
     // Update is called once per frame
     void Update()
     {
         Vector3 newPos = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        newPos.Normalize();
+        //newPos.Normalize();
 
         Vector3 newRot = this.transform.position + newPos;
 
@@ -23,10 +28,23 @@ public class PlayerMovement : MonoBehaviour
         // Apply the rotation to the player on the y-axis only
         //transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
 
-        this.transform.rotation = Quaternion.Lerp(this.transform.rotation,Quaternion.Euler(new Vector3(0,targetAngle,0)), rotationSpeed * Time.deltaTime);
+        //this.transform.rotation = Quaternion.Lerp(this.transform.rotation,Quaternion.Euler(new Vector3(0,targetAngle,0)), rotationSpeed * Time.deltaTime);
 
-        newPos *= movementSpeed * Time.deltaTime;
-        this.transform.position += newPos;
+        //newPos *= movementSpeed * Time.deltaTime;
+        //this.transform.position += newPos;
+
+        // Apply the movement direction to the character.
+        if (newPos.magnitude > 0)
+        {
+            moveDirection = newPos.normalized * movementSpeed;
+        }
+        else
+        {
+            moveDirection = Vector3.zero;
+        }
+
+        // Move the character.
+        controller.Move(moveDirection * Time.deltaTime);
 
 
         //this.transform.
