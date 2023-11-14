b658fb769de836b4d176a8773a167f4e6e54109f
Lylithrea <Ezra@easysee.nl>
2023-11-14 17:37:40 +0100

    fmod implemented and working

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+
+    public FMODUnity.EventReference soundEvent;
+
+    void Start()
+    {
+        //FMODUnity.RuntimeManager.PlayOneShot(soundEvent, transform.position);
+
+    }
+}
